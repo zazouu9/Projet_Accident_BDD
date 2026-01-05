8a26d82d79ce81831756b76faea623d787473175
manon favrel <mafavrel@admin14.istic.univ-rennes1.fr>
2026-01-05 12:02:15 +0100

    stat ajout accident/mois

--- stat_1.py
+++ stat_1.py
@@ -31,6 +31,25 @@ def main():
     )
     print(" accidents_par_heure.csv est bien créé")
 
+    # 2 ACCIDENTS PAR MOIS
+    
+    query_mois = """
+    SELECT
+        mois,
+        COUNT(DISTINCT Num_Acc) AS nb_accidents
+    FROM caracteristiques
+    WHERE mois IS NOT NULL AND mois != ''
+    GROUP BY mois
+    ORDER BY CAST(mois AS INTEGER);
+    """
+    df_mois = pd.read_sql_query(query_mois, conn)
+    df_mois.to_csv(
+        os.path.join(OUT_DIR, "accidents_par_mois.csv"),
+        index=False,
+        encoding="utf-8"
+    )
+    print("accidents_par_mois.csv bien créé")
+
 
 
 if __name__ == "__main__":
